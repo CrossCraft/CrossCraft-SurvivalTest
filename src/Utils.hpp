1f46dae2401a28ea95321f34962aca375b80c81d
Nathan Bourgeois <IridescentRosesFall@gmail.com>
2022-08-13 14:55:24 -0400

    Survival Test folder

--- src/Utils.hpp
+++ src/Utils.hpp
@@ -8,7 +8,7 @@
 #define PLATFORM_FILE_PREFIX std::string("")
 #define PLATFORM_APP_FILE_PREFIX std::string("")
 #else
-#define PLATFORM_FILE_PREFIX std::string("ux0:/data/CrossCraft-Classic/")
+#define PLATFORM_FILE_PREFIX std::string("ux0:/data/CrossCraft-ST/")
 #define PLATFORM_APP_FILE_PREFIX std::string("app0:/")
 #endif
 
@@ -18,39 +18,43 @@
 #include <sys/types.h>
 #endif
 
-namespace CrossCraft {
-using namespace Stardust_Celeste;
-inline void netInit() {
+namespace CrossCraft
+{
+    using namespace Stardust_Celeste;
+    inline void netInit()
+    {
 #if PSP
-    Network::NetworkDriver::get().initGUI();
+        Network::NetworkDriver::get().initGUI();
 #elif BUILD_PLAT == BUILD_VITA
-    Network::NetworkDriver::get().init();
+        Network::NetworkDriver::get().init();
 #endif
-}
+    }
 
-inline void createDirs() {
+    inline void createDirs()
+    {
 #if BUILD_PLAT == BUILD_VITA
-    sceIoMkdir("ux0:/data/CrossCraft-Classic", 0777);
-    sceIoMkdir("ux0:/data/CrossCraft-Classic/texturepacks", 0777);
-    Utilities::Logger::get_app_log()->flush_output = true;
-    {
-        std::ifstream src("app0:/texturepacks/default.zip", std::ios::binary);
-        std::ofstream dst(
-            "ux0:/data/CrossCraft-Classic/texturepacks/default.zip",
-            std::ios::binary);
-        dst << src.rdbuf();
-    }
-#elif BUILD_PLAT == BUILD_PSP
-    sceIoMkdir("./texturepacks", 0777);
-    {
-        std::ifstream src("./default.zip", std::ios::binary);
-        if (src.is_open()) {
-            std::ofstream dst("./texturepacks/default.zip", std::ios::binary);
+        sceIoMkdir("ux0:/data/CrossCraft-ST", 0777);
+        sceIoMkdir("ux0:/data/CrossCraft-ST/texturepacks", 0777);
+        Utilities::Logger::get_app_log()->flush_output = true;
+        {
+            std::ifstream src("app0:/texturepacks/default.zip", std::ios::binary);
+            std::ofstream dst(
+                "ux0:/data/CrossCraft-ST/texturepacks/default.zip",
+                std::ios::binary);
             dst << src.rdbuf();
-            src.close();
-            sceIoRemove("./default.zip");
         }
-    }
+#elif BUILD_PLAT == BUILD_PSP
+        sceIoMkdir("./texturepacks", 0777);
+        {
+            std::ifstream src("./default.zip", std::ios::binary);
+            if (src.is_open())
+            {
+                std::ofstream dst("./texturepacks/default.zip", std::ios::binary);
+                dst << src.rdbuf();
+                src.close();
+                sceIoRemove("./default.zip");
+            }
+        }
 #endif
-}
+    }
 } // namespace CrossCraft
