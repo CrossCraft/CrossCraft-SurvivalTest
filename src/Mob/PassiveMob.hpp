7f92440123880e6323badbed9d33aeb61562c9b0
Nathan Bourgeois <IridescentRosesFall@gmail.com>
2022-08-17 20:13:46 -0400

    Basic Passive AI

--- src/Mob/PassiveMob.hpp
+++ src/Mob/PassiveMob.hpp
@@ -6,6 +6,7 @@ namespace CrossCraft
     class PassiveMob : public Mob
     {
     public:
+        float dirChangeTime;
         PassiveMob() = default;
         virtual ~PassiveMob() = default;
         void update(float dt, Player *p, World* w);
