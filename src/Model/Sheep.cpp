ad655ddfe25389c4ba543a3bc26fd2333546e6e6
Nathan Bourgeois <iridecentrosesfall@gmail.com>
2022-08-19 15:59:47 -0400

    Quadripedal Animations

--- src/Model/Sheep.cpp
+++ src/Model/Sheep.cpp
@@ -2,70 +2,67 @@
 #include "../TexturePackManager.hpp"
 #include "../Utils.hpp"
 #include <yaml-cpp/yaml.h>
-namespace CrossCraft
-{
+namespace CrossCraft {
 
-    Sheep::Sheep()
+Sheep::Sheep() {
     {
-        {
-            YAML::Node config =
-                YAML::LoadFile(PLATFORM_APP_FILE_PREFIX + "models/sheep.yaml");
-
-            auto tSize = config["model"][0]["texsize"];
-            auto texSize = glm::vec2(tSize[0].as<float>(), tSize[1].as<float>());
-
-            leg.load(config["model"][1], texSize);
-            torso.load(config["model"][2], texSize);
-            head.load(config["model"][3], texSize);
-
-            tex = TexturePackManager::get().load_texture(
-                "assets/mob/sheep.png", SC_TEX_FILTER_NEAREST, SC_TEX_FILTER_NEAREST,
-                false, false);
-            tex2 = TexturePackManager::get().load_texture(
-                "assets/mob/sheep_fur.png", SC_TEX_FILTER_NEAREST, SC_TEX_FILTER_NEAREST,
-                false, false);
-        }
-
         YAML::Node config =
-            YAML::LoadFile(PLATFORM_APP_FILE_PREFIX + "models/sheep_fur.yaml");
+            YAML::LoadFile(PLATFORM_APP_FILE_PREFIX + "models/sheep.yaml");
 
         auto tSize = config["model"][0]["texsize"];
         auto texSize = glm::vec2(tSize[0].as<float>(), tSize[1].as<float>());
 
-        legWool.load(config["model"][1], texSize);
-        torsoWool.load(config["model"][2], texSize);
-        headWool.load(config["model"][3], texSize);
+        leg.load(config["model"][1], texSize);
+        torso.load(config["model"][2], texSize);
+        head.load(config["model"][3], texSize);
+
+        tex = TexturePackManager::get().load_texture(
+            "assets/mob/sheep.png", SC_TEX_FILTER_NEAREST,
+            SC_TEX_FILTER_NEAREST, false, false);
+        tex2 = TexturePackManager::get().load_texture(
+            "assets/mob/sheep_fur.png", SC_TEX_FILTER_NEAREST,
+            SC_TEX_FILTER_NEAREST, false, false);
     }
 
-    Sheep::~Sheep() {}
+    YAML::Node config =
+        YAML::LoadFile(PLATFORM_APP_FILE_PREFIX + "models/sheep_fur.yaml");
 
-    void Sheep::draw(SheepData *sd)
-    {
-        auto ctx = &Rendering::RenderContext::get();
-        ctx->matrix_clear();
-        ctx->matrix_translate({sd->pos.x, sd->pos.y - 0.75f, sd->pos.z});
-        ctx->matrix_rotate(glm::vec3(sd->rot, 0.0f));
-        ctx->matrix_scale({0.9f, 0.9f, 0.9f});
+    auto tSize = config["model"][0]["texsize"];
+    auto texSize = glm::vec2(tSize[0].as<float>(), tSize[1].as<float>());
 
-        Rendering::TextureManager::get().bind_texture(tex);
+    legWool.load(config["model"][1], texSize);
+    torsoWool.load(config["model"][2], texSize);
+    headWool.load(config["model"][3], texSize);
+}
 
-        head.draw({0.0f, 0.0f, 0.0f}, {0, 0, 0}, {1, 1, 1});
-        torso.draw({-0.1875f, -0.25f, 0.0f}, {0, 0, 90}, {1, 1, 1});
-        leg.draw({0.0f, -0.8125f, 0.1875f}, {0, 180, 0}, {-1, 1, 1});
-        leg.draw({0.0f, -0.8125f, -0.1875f}, {0, 0, 0}, {1, 1, 1});
-        leg.draw({ 0.65625, -0.8125f, 0.1875f }, { 0, 180, 0 }, { -1, 1, -1 });
-        leg.draw({ 0.65625, -0.8125f, -0.175f}, {0, 0, 0}, {1, 1, -1});
+Sheep::~Sheep() {}
 
-        if (sd->hasWool)
-        {
-            Rendering::TextureManager::get().bind_texture(tex2);
-            headWool.draw({-0.1875f, 0.0f, 0.0f}, {0, 0, 0}, {1, 1, 1});
-            torsoWool.draw({-0.1875f, -0.25f, 0.0f}, {0, 0, 90}, {1, 1, 1});
-            legWool.draw({0.0f, -0.8125f, 0.1875f}, {0, 180, 0}, {-1, 1, 1});
-            legWool.draw({0.0f, -0.8125f, -0.1875f}, {0, 0, 0}, {1, 1, 1});
-            legWool.draw({0.65625, -0.8125f, 0.1875f}, {0, 180, 0}, {-1, 1, -1});
-            legWool.draw({0.65625, -0.8125f, -0.175f}, {0, 0, 0}, {1, 1, -1});
-        }
-        ctx->matrix_clear();
+void Sheep::draw(SheepData *sd) {
+    auto ctx = &Rendering::RenderContext::get();
+    ctx->matrix_clear();
+    ctx->matrix_translate({sd->pos.x, sd->pos.y - 0.75f, sd->pos.z});
+    ctx->matrix_rotate(glm::vec3(sd->rot, 0.0f));
+    ctx->matrix_scale({0.9f, 0.9f, 0.9f});
+
+    Rendering::TextureManager::get().bind_texture(tex);
+    auto sval = sinf(sd->animationTime * 3.14159f * 4.0f / 3.0f) * 10.0f;
+
+    head.draw({0.0f, 0.0f, 0.0f}, {0, 0, 0}, {1, 1, 1});
+    torso.draw({-0.1875f, -0.25f, 0.0f}, {0, 0, 90}, {1, 1, 1});
+    leg.draw({0.0f, -0.4375f, 0.1875f}, {0, 180, sval}, {-1, 1, 1});
+    leg.draw({0.0f, -0.4375f, -0.1875f}, {0, 0, sval}, {1, 1, 1});
+    leg.draw({0.625, -0.4375f, 0.1875f}, {0, 180, -sval}, {-1, 1, -1});
+    leg.draw({0.625, -0.4375f, -0.1875f}, {0, 0, -sval}, {1, 1, -1});
+
+    if (sd->hasWool) {
+        Rendering::TextureManager::get().bind_texture(tex2);
+        headWool.draw({-0.1875f, 0.0f, 0.0f}, {0, 0, 0}, {1, 1, 1});
+        torsoWool.draw({-0.1875f, -0.25f, 0.0f}, {0, 0, 90}, {1, 1, 1});
+        legWool.draw({0.0f, -0.4375f, 0.1875f}, {0, 180, sval}, {-1, 1, 1});
+        legWool.draw({0.0f, -0.4375f, -0.1875f}, {0, 0, sval}, {1, 1, 1});
+        legWool.draw({0.625, -0.4375f, 0.1875f}, {0, 180, -sval}, {-1, 1, -1});
+        legWool.draw({0.625, -0.4375f, -0.1875f}, {0, 0, -sval}, {1, 1, -1});
     }
+    ctx->matrix_clear();
+}
 } // namespace CrossCraft
