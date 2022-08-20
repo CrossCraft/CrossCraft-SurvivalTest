823efe59393599c8c779a64664606dbe960666ad
Nathan Bourgeois <iridecentrosesfall@gmail.com>
2022-08-20 11:51:00 -0400

    Update Vita build

--- src/Utils.hpp
+++ src/Utils.hpp
@@ -18,59 +18,47 @@
 #include <sys/types.h>
 #endif
 
-namespace CrossCraft
-{
-    using namespace Stardust_Celeste;
-    inline void netInit()
-    {
+namespace CrossCraft {
+using namespace Stardust_Celeste;
+inline void netInit() {
 #if PSP
-        Network::NetworkDriver::get().initGUI();
+    Network::NetworkDriver::get().initGUI();
 #elif BUILD_PLAT == BUILD_VITA
-        Network::NetworkDriver::get().init();
+    Network::NetworkDriver::get().init();
 #endif
-    }
+}
 
 #if BUILD_PLAT == BUILD_VITA || BUILD_PLAT == BUILD_PSP
-    inline void sceIoCopy(std::string source, std::string dest, bool remove)
+inline void sceIoCopy(std::string source, std::string dest, bool remove) {
+    std::ifstream src(source, std::ios::binary);
+    if (src.is_open()) {
+        std::ofstream dst(dest, std::ios::binary);
+        dst << src.rdbuf();
+        src.close();
+
+        if (remove)
+            sceIoRemove(source.c_str());
+    }
+}
+#endif
+
+inline void createDirs() {
+#if BUILD_PLAT == BUILD_VITA
+    sceIoMkdir("ux0:/data/CrossCraft-ST", 0777);
+    sceIoMkdir("ux0:/data/CrossCraft-ST/resourcepacks", 0777);
+    Utilities::Logger::get_app_log()->flush_output = true;
+    Utilities::Logger::get_core_log()->flush_output = true;
+#elif BUILD_PLAT == BUILD_PSP
+    sceIoMkdir("./resourcepacks", 0777);
     {
-        std::ifstream src(source, std::ios::binary);
-        if (src.is_open())
-        {
-            std::ofstream dst(dest, std::ios::binary);
+        std::ifstream src("./default.zip", std::ios::binary);
+        if (src.is_open()) {
+            std::ofstream dst("./resourcepacks/default.zip", std::ios::binary);
             dst << src.rdbuf();
             src.close();
-
-            if (remove)
-                sceIoRemove(source.c_str());
+            sceIoRemove("./default.zip");
         }
     }
 #endif
-
-    inline void createDirs()
-    {
-#if BUILD_PLAT == BUILD_VITA
-        sceIoMkdir("ux0:/data/CrossCraft-ST", 0777);
-        sceIoMkdir("ux0:/data/CrossCraft-ST/resourcepacks", 0777);
-        Utilities::Logger::get_app_log()->flush_output = true;
-        {
-            std::ifstream src("app0:/resourcepacks/default.zip", std::ios::binary);
-            std::ofstream dst(
-                "ux0:/data/CrossCraft-ST/resourcepacks/default.zip",
-                std::ios::binary);
-            dst << src.rdbuf();
-        }
-#elif BUILD_PLAT == BUILD_PSP
-        sceIoMkdir("./resourcepacks", 0777);
-        {
-            std::ifstream src("./default.zip", std::ios::binary);
-            if (src.is_open())
-            {
-                std::ofstream dst("./resourcepacks/default.zip", std::ios::binary);
-                dst << src.rdbuf();
-                src.close();
-                sceIoRemove("./default.zip");
-            }
-        }
-#endif
-    }
+}
 } // namespace CrossCraft
